d41390b2483e81b8dd127daf3398a094a5b95ebb
[estebans0] <[estebanjs029@gmail.com]>
2022-05-04 18:08:44 -0600

    Inicio repo

---
+++ TP2Funciones.py
@@ -0,0 +1,6 @@
+# Elaborado por: Juan Ceballos y Esteban Solano
+# Fecha de Creación: 04/05/2022 04:53pm
+# Fecha de última Modificación: XX/XX/2022 XX:XXxm
+# Versión: 3.10.2
+
+# Definición de funciones
---
+++ TP2Menu.py
@@ -0,0 +1,12 @@
+# Elaborado por: Juan Ceballos y Esteban Solano
+# Fecha de Creación: 04/05/2022 04:53pm
+# Fecha de última Modificación: XX/XX/2022 XX:XXxm
+# Versión: 3.10.2
+
+# Importación de librerías
+from TP2Funciones import *
+
+# Definición de validaciones
+
+
+# Programa Principal
